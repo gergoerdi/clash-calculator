470b903368e66e6fdf7f64d6e82250012795477e
Gergo ERDI <gergo@erdi.hu>
2021-10-27 16:32:39 +0800

    When targeting Xilin 7 boards, optionally use SymbiFlow

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,42 +1,56 @@
 {-# LANGUAGE RecordWildCards, NumericUnderscores #-}
 import Clash.Shake
-import Clash.Shake.Xilinx
+import Clash.Shake.Xilinx as Xilinx
+import qualified Clash.Shake.SymbiFlow as SymbiFlow
 
 import Development.Shake
 import Development.Shake.FilePath
+import System.Console.GetOpt
 import Data.Foldable (forM_)
-
-targets =
-    [ ("nexys-a7-50t", xilinxVivado nexysA750T, 100_000_000)
-    , ("papilio-pro",  xilinxISE papilioPro,     32_000_000)
-    , ("papilio-one",  xilinxISE papilioOne,     32_000_000)
-    ]
 
 outDir :: FilePath
 outDir = "_build"
 
+data Flags = UseSymbiFlow deriving Eq
+
+flags = [Option "" ["symbiflow"] (NoArg $ Right UseSymbiFlow) "Use SymbiFlow instead of vendor toolchain"]
+
 main :: IO ()
-main = shakeArgs shakeOptions{ shakeFiles = outDir } $ do
+main = shakeArgsWith shakeOptions{ shakeFiles = outDir } flags $ \flags targets -> pure $ Just $ do
     useConfig "build.mk"
 
-    phony "clean" $ do
-        putNormal $ "Cleaning files in " <> outDir
-        removeFilesAfter outDir [ "//*" ]
+    let useSymbiFlow = UseSymbiFlow `elem` flags
+        xilinx7
+          | useSymbiFlow = SymbiFlow.xilinx7
+          | otherwise = Xilinx.vivado
 
-    forM_ targets $ \(name, synth, clock) -> do
-        let targetDir = outDir </> name
+    let boards =
+            [ ("nexys-a7-50t", xilinx7 nexysA750T,    100_000_000)
+            , ("papilio-pro",  Xilinx.ise papilioPro,  32_000_000)
+            , ("papilio-one",  Xilinx.ise papilioOne,  32_000_000)
+            ]
 
-        kit@ClashKit{..} <- clashRules (targetDir </> "clash") Verilog
-            [ "src" ]
-            "Calculator"
-            [ "-Wno-partial-type-signatures"
-            , "-Wunused-imports"
-            , "-D__NATIVE_CLOCK__=" <> show clock
-            ] $
-            return ()
-        SynthKit{..} <- synth kit (targetDir </> "synth") ("target" </> name) "Top"
+    let rules = do
+        phony "clean" $ do
+            putNormal $ "Cleaning files in " <> outDir
+            removeFilesAfter outDir [ "//*" ]
 
-        mapM_ (uncurry $ nestedPhony name) $
-          ("clashi", clash ["--interactive", "src/Calculator.hs"]) :
-          ("bitfile", need [bitfile]):
-          phonies
+        forM_ boards $ \(name, synth, clock) -> do
+            let targetDir = outDir </> name
+
+            kit@ClashKit{..} <- clashRules (targetDir </> "clash") Verilog
+                [ "src" ]
+                "Calculator"
+                [ "-Wno-partial-type-signatures"
+                , "-Wunused-imports"
+                , "-D__NATIVE_CLOCK__=" <> show clock
+                ] $
+                return ()
+            SynthKit{..} <- synth kit (targetDir </> "synth") ("target" </> name) "Top"
+
+            mapM_ (uncurry $ nestedPhony name) $
+              ("clashi", clash ["--interactive", "src/Calculator.hs"]) :
+              ("bitfile", need [bitfile]):
+              phonies
+
+    if null targets then rules else want targets >> withoutActions rules
